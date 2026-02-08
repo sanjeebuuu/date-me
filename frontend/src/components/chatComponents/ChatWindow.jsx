e244902ae17d68aa7689bec8912ddf3c079e1b68
sanjeebuuu <sanjibpoudel99@gmail.com>
2026-02-08 21:58:31 +0545

    Update ChatWindow.jsx

--- frontend/src/components/chatComponents/ChatWindow.jsx
+++ frontend/src/components/chatComponents/ChatWindow.jsx
@@ -28,7 +28,7 @@ function ChatWindow({ chatId }) {
         fetchMessages();
         const token = localStorage.getItem("token")
         const ws = new WebSocket(
-            `ws://date-me-tbh2.onrender.com/ws/chat/${chatId}?token=${token}`
+            `wss://date-me-tbh2.onrender.com/ws/chat/${chatId}?token=${token}`
         )
 
         ws.onmessage = (event) => {
